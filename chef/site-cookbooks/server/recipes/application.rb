83c602f4f3445e66ed0e76f23060891fafe09540
Michael <michael.schiller@gmx.de>
2011-11-30 20:58:55 +0100

    added folder for unix sockets of unicorn

--- chef/site-cookbooks/server/recipes/application.rb
+++ chef/site-cookbooks/server/recipes/application.rb
@@ -5,3 +5,11 @@ directory "/var/projects/" do
   mode "0755"
   action :create
 end
+
+# unicorn sockets
+directory "/tmp/sockets/" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  mode "0755"
+  action :create
+end
