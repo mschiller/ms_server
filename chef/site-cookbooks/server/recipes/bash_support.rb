9bc8c13a10a6dcc0a39b03d95b670221ab999f08
Michael <michael.schiller@gmx.de>
2011-12-14 12:18:36 +0100

    added better bash support

--- chef/site-cookbooks/server/recipes/bash_support.rb
+++ chef/site-cookbooks/server/recipes/bash_support.rb
@@ -1,10 +1,43 @@
+home = "/home/#{node[:deployer_user][:username]}"
 
-template "/home/#{node[:deployer_user][:username]}/.inputrc" do
+template "#{home}/.inputrc" do
   owner node[:deployer_user][:username]
   group node[:deployer_user][:username]
-  source "inputrc.erb"
+  source "bash/inputrc.erb"
   mode 0640
-  not_if do
-    File.exists?("/home/#{node[:deployer_user][:username]}/.inputrc")
-  end
+end
+
+template "#{home}/.bash_profile" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  source "bash/bash_profile.erb"
+  mode 0640
+end
+
+template "#{home}/.git-completion.bash" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  source "bash/git-completion.bash.erb"
+  mode 0640
+end
+
+directory "#{home}/.lightning" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  mode "0755"
+  action :create
+end
+
+template "#{home}/.lightning/functions.sh" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  source "bash/functions.sh.erb"
+  mode 0640
+end
+
+template "#{home}/.lightningrc" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  source "bash/lightningrc.erb"
+  mode 0640
 end
