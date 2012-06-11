6d293da20431b95dfc97809a3e3e09de5c731016
Michael <michael.schiller@gmx.de>
2012-06-11 07:13:54 +0200

    added ppa for nginx to get always the current nginx

--- chef/site-cookbooks/server/recipes/default.rb
+++ chef/site-cookbooks/server/recipes/default.rb
@@ -5,7 +5,7 @@ require_recipe "git"
 require_recipe "screen"
 include_recipe "java::openjdk"
 
-%w(libxml++2.6-dev libxslt1-dev zip libssl-dev libxml2-dev  libreadline6-dev libghc6-curl-dev curl libqt4-dev nmap imagemagick).each do |pkg|
+%w(libxml++2.6-dev libxslt1-dev zip libssl-dev libxml2-dev  libreadline6-dev libghc6-curl-dev curl libqt4-dev nmap imagemagick python-software-properties).each do |pkg|
   package pkg
 end
 
@@ -45,7 +45,11 @@ iptables_rule "drop_and_logging" do
   source 'iptables/drop_and_logging.erb'
 end
 
+execute "add source for newest nginx" do
+  command "add-apt-repository ppa:nginx/stable"
+end
 require_recipe "nginx"
+
 require_recipe "unicorn"
 require_recipe "memcached"
 require_recipe "imagemagick::rmagick"
@@ -101,6 +105,16 @@ directory "#{node[:nginx][:dir]}/cert" do
 end
 node[:certificates].each do |cert|
   name = cert[:name]
+
+  #With Startssl and Nginx:
+  #ssl  on;
+  #ssl_certificate  /etc/nginx/ssl/ssl.crt;
+  #ssl_certificate_key  /etc/nginx/ssl/blog.key;
+  #
+  #Nginx doesn’t do SSL certificate chaining like Apache2 does. In order to get the ca.pem and sub.class1.server.ca.pem onto your install just append the two files to your certificate file.
+  #openssl rsa -in ssl.key -out /etc/nginx/conf/ssl.key
+  #curl http://www.startssl.com/certs/sub.class1.server.ca.pem >>ssl.crt
+  #curl http://www.startssl.com/certs/ca.pem >>ssl.crt
 
   cert[:files].each do |key, value|
     user "root"
