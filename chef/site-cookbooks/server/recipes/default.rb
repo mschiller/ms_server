cb249bac82ea77772f4ca92bbff1866569d2091c
Michael <michael.schiller@gmx.de>
2014-08-22 23:57:35 +0200

    ruby 2.0.0-p481

--- chef/site-cookbooks/server/recipes/default.rb
+++ chef/site-cookbooks/server/recipes/default.rb
@@ -61,10 +61,10 @@ include_recipe "rbenv::default"
 include_recipe "rbenv::ruby_build"
 include_recipe "rbenv::rbenv_vars"
 
-rbenv_ruby "2.0.0-p353"
+rbenv_ruby "2.0.0-p481"
 
 execute "set as default" do
-  command "rbenv global 2.0.0-p353"
+  command "rbenv global 2.0.0-p481"
 end
 
 rbenv_install_gem 'bundler' do
