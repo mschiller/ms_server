5ab95c4aa6bf9033117d92ec952d0d90d85a63e4
Michael <michael.schiller@gmx.de>
2014-09-27 13:47:15 +0200

    fixed backup

--- chef/site-cookbooks/server/recipes/backup.rb
+++ chef/site-cookbooks/server/recipes/backup.rb
@@ -18,6 +18,20 @@ package 'bzip2' do
   action :install
 end
 
+directory "/home/#{node[:deployer_user][:username]}/Backup" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  mode "0755"
+  action :create
+end
+
+directory "/home/#{node[:deployer_user][:username]}/Backup/log" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
+  mode "0755"
+  action :create
+end
+
 directory "/home/#{node[:deployer_user][:username]}/bin/" do
   owner node[:deployer_user][:username]
   group node[:deployer_user][:username]
@@ -32,7 +46,7 @@ directory "/home/#{node[:deployer_user][:username]}/scripts/" do
 end
 
 execute "add mysql backup user" do
-  command "mysql -u#{node.mysql.server_root_username} -p#{node.mysql.server_root_password} -e \"GRANT SELECT, SHOW VIEW ON *.* TO '#{node.backup.database_username}'@'#{node.backup.from_host}' IDENTIFIED BY '#{node.backup.database_password}';\""
+  command "mysql -u#{node[:mysql][:server_root_username]} -p#{node[:mysql][:server_root_password]} -e \"GRANT SELECT, SHOW VIEW ON *.* TO '#{node[:backup][:database_username]}'@'#{node[:backup][:from_host]}' IDENTIFIED BY '#{node[:backup][:database_password]}';\""
   action :run
 end
 
@@ -43,28 +57,34 @@ template "/home/#{node[:deployer_user][:username]}/bin/backup.sh" do
   mode "0700"
   variables(
     {
-      :backuped_projects => node.backup.backuped_projects
+      backuped_projects: node[:backup][:backuped_projects]
     }.merge!(
       :config_file => "/home/#{node[:deployer_user][:username]}/scripts/backup.rb",
-      :user => node.deployer_user.username
+      :user => node[:deployer_user][:username]
     )
   )
 end
 
-template "/home/#{node.deployer_user.username}/scripts/backup.rb" do
-  owner node.deployer_user.username
-  group node.deployer_user.username
+template "/home/#{node[:deployer_user][:username]}/scripts/backup.rb" do
+  owner node[:deployer_user][:username]
+  group node[:deployer_user][:username]
   source "backup.rb.erb"
   variables(
-    node.backup.to_hash
+    node[:backup].to_hash.merge!(
+        {
+          mail: node[:backup][:mail],
+          backuped_projects: node[:backup][:backuped_projects]
+        }
+    )
   )
 end
 
 cron "cron task for backup" do
-  user node.deployer_user.username
-  hour "5"
-  minute "0"
+  user node[:deployer_user][:username]
+  hour '5'
+  minute '0'
+  #weekday '1'
   command "/home/#{node[:deployer_user][:username]}/bin/backup.sh 2>&1 >/dev/null"
-  mailto node.backup.mail.to
+  mailto node[:backup][:mail][:to]
   only_if do File.exist?("/home/#{node[:deployer_user][:username]}/bin/backup.sh") end
 end
